3a1b296a85bb260f7eded23938e6cdbcec5a9a63
natux1 <t170d516@gunma-u.ac.jp>
2021-07-16 04:54:38 +0900

    GUI 実装

--- tutorial.py
+++ tutorial.py
@@ -3,9 +3,11 @@ import PySimpleGUI as sg
 sg.theme('DarkTeal7')   # デザインテーマの設定
 
 # ウィンドウに配置するコンポーネント
-layout = [  [sg.Text('TagFreq(Hz):',size=(15,1)),sg.Input(size=(20,1)),sg.Text('Diopter(Dptr):',size=(15,1)),sg.Input(size=(20,1))],
-            [sg.Text('ExposureTime(us):',size=(15,1)), sg.InputText(size=(20,1)),sg.Text('MaxDptr(|Dptr|):',size=(15,1)),sg.Input(size=(20,1))],
-            [sg.Button('OK'), sg.Button('キャンセル')] ]
+layout = [  [sg.Text('TagFreq(Hz):',size=(15,1)),sg.Input(size=(20,1),key='freq'),sg.Text('Diopter(Dptr):',size=(15,1)),sg.Input(size=(20,1),key='dptr')],
+            [sg.Text('ExposureTime(us):',size=(15,1)), sg.InputText(size=(20,1),key='Extime'),sg.Text('MaxDptr(|Dptr|):',size=(15,1)),sg.Input(size=(20,1),key='maxdptr')],
+            [sg.Button('OK',key='cal'), sg.Button('リセット',key='reset')],
+            [sg.Output(size=(50,20),key='outlines')]
+        ]
 
 # ウィンドウの生成
 window = sg.Window('TagParameterFinder', layout)
@@ -13,9 +15,19 @@ window = sg.Window('TagParameterFinder', layout)
 # イベントループ
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'キャンセル':
+    if event == sg.WIN_CLOSED:
         break
-    elif event == 'OK':
-        print('あなたが入力した値： ', values[0])
-
+    
+    if event == 'cal':
+        window['outlines'].update('')
+        
+        print('Exposure timing:',3.12,'-',3.625)
+        print('------------------------',hex(312),'-',hex(362),"(resolution 10ns)")
+    if event == 'reset':
+        window['outlines'].update('')
+        
+        
+    
+    
+    
 window.close()
