0a9f927bc17192e55505da77c3d9e01ab28f50e2
natux1 <t170d516@gunma-u.ac.jp>
2021-07-22 20:51:48 +0900

    GUI実装

--- Calculatetaglens.py
+++ Calculatetaglens.py
@@ -2,12 +2,16 @@ import numpy as np
 import math
 
 def exposuretiming(freq,dptr,dptrmax):
-    
-    Ts = 1/freq
-    theta = math.asin(dptr/dptrmax)
-    theta2 = math.pi - theta 
-    t1=(Ts*theta)/(2*math.pi)
-    t2=(Ts*theta2)/(2*math.pi)
-    Time = [t1,t2]
+    try:
+        Ts = 1/freq
+        theta = math.asin(dptr/dptrmax)
+        theta2 = math.pi - theta 
+        t1=(Ts*theta)/(2*math.pi)
+        t2=(Ts*theta2)/(2*math.pi)
+        Time = [t1,t2]
+    except ZeroDivisionError as e:
+        print('catch ZeroDivisionError:', e)
+    except TypeError as e:
+        print('catch TypeError:', e)
     
     return Time    
--- tutorial.py
+++ tutorial.py
@@ -1,19 +1,22 @@
 import PySimpleGUI as sg
+import sys
+import os
 import Calculatetaglens
 
-sg.theme('DarkTeal7')   # デザインテーマの設定
+def resource_path(relative):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative)
+    return os.path.join(relative)
 
-# ウィンドウに配置するコンポーネント
+sg.theme('DarkTeal7')
+
 layout = [  [sg.Text('TagFreq(Hz):',size=(15,1)),sg.Input(size=(20,1),key='freq'),sg.Text('Diopter(Dptr):',size=(15,1)),sg.Input(size=(20,1),key='dptr')],
-            [sg.Text('ExposureTime(us):',size=(15,1)), sg.InputText(size=(20,1),key='Extime'),sg.Text('MaxDptr(|Dptr|):',size=(15,1)),sg.Input(size=(20,1),key='maxdptr')],
-            [sg.Button('OK',key='cal'), sg.Button('リセット',key='reset')],
+            [sg.Text('ExposureTime(ns):',size=(15,1)), sg.InputText(size=(20,1),key='Extime'),sg.Text('MaxDptr(|Dptr|):',size=(15,1)),sg.Input(size=(20,1),key='maxdptr')],
+            [sg.Button('OK',key='cal'), sg.Button('reset',key='reset')],
             [sg.Output(size=(50,20),key='outlines')]
         ]
 
-# ウィンドウの生成
 window = sg.Window('TagParameterFinder', layout)
-
-# イベントループ
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
@@ -21,23 +24,25 @@ while True:
     
     if event == 'cal':
         window['outlines'].update('')
-        freq = float(values['freq'])
-        dptr = float(values['dptr'])
-        maxdptr = float(values['maxdptr'])
-        exposuretime = float(values['Extime'])
-        
-        time = Calculatetaglens.exposuretiming(freq,dptr,maxdptr)
-        t1 = [time[0]*10**6,time[0]*10**6+exposuretime]
-        t2 = [time[1]*10**6,time[1]*10**6+exposuretime]
-        
-        t1hex = [round(time[0]/(10*10**-9)),round((time[0]+exposuretime/(10**6))/(10*10**-9))]
-        t2hex = [round(time[1]/(10*10**-9)),round((time[1]+exposuretime/(10**6))/(10*10**-9))]
-        
-        print(f'Exposure timing(1):{t1[0]:.4f}us -- {t1[1]:.4f}us')
-        print(f'-------------------{t1hex[0]:x} -- {t1hex[1]:x}')
-        print(f'Exposure timing(2):{t2[0]:.4f}us -- {t2[1]:.4f}us')
-        print(f'-------------------{t2hex[0]:.x} -- {t2hex[1]:.x}')
-        print('------------------------',hex(312),'-',hex(362),"(resolution 10ns)")
+        try:
+            freq = float(values['freq'])
+            dptr = float(values['dptr'])
+            maxdptr = abs(float(values['maxdptr']))
+            exposuretime = float(values['Extime'])
+        except ValueError:
+            sg.PopupCancel('There is non-numeric data!',title='Error')
+        else:
+            time = Calculatetaglens.exposuretiming(freq,dptr,maxdptr)
+            t1 = [time[0]*10**6,time[0]*10**6+exposuretime/1000]
+            t2 = [time[1]*10**6,time[1]*10**6+exposuretime/1000]
+            t1hex = [round(time[0]/(10*10**-9)),round((time[0]+exposuretime/(10**9))/(10*10**-9))]
+            t2hex = [round(time[1]/(10*10**-9)),round((time[1]+exposuretime/(10**9))/(10*10**-9))]
+            
+            print(f'Exposure timing(1):{t1[0]:.4f}us -- {t1[1]:.4f}us')
+            print(f'--------Hex-------------      0x{t1hex[0]:x} -- 0x{t1hex[1]:x}',sep='----')
+            print(f'Exposure timing(2):{t2[0]:.4f}us -- {t2[1]:.4f}us')
+            print(f'--------Hex-------------      0x{t2hex[0]:x} -- 0x{t2hex[1]:x}',sep='----')
+            
     if event == 'reset':
         window['outlines'].update('')
         
